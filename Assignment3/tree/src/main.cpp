daafc1f856e4339beec4623a35794ea49adfa6e4
Jasper Blank <j.j.blank@student.utwente.nl>
2024-03-01 19:16:23 +0100

    Some tree updates

--- Assignment3/tree/src/main.cpp
+++ Assignment3/tree/src/main.cpp
@@ -5,7 +5,14 @@
 // License     :  N.A. or opensource license like LGPL
 // Description : 
 //==============================================================
+#include <iostream>
+#include "../include/Tree.h"
+#include "../include/TreeNode.h"
 
 int main() {
+
+    std::cout << "Creating tree";
+    Tree<int> IntTree;
+
     return 0;
 }
