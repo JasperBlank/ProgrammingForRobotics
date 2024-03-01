13bcabcf50f9aacd98848eec648fe82514d2ba39
Jasper Blank <j.j.blank@student.utwente.nl>
2024-03-01 19:27:26 +0100

    Basic Tree overvieuw

--- Assignment3/tree/src/main.cpp
+++ Assignment3/tree/src/main.cpp
@@ -14,5 +14,20 @@ int main() {
     std::cout << "Creating tree";
     Tree<int> IntTree;
 
+    IntTree.insertNode(9);
+    IntTree.insertNode(8);
+    IntTree.insertNode(7);
+    IntTree.insertNode(11);
+    IntTree.insertNode(5);
+    IntTree.insertNode(4);
+    IntTree.insertNode(10);
+
+    std::cout << "\n PreOrderTraversal: ";
+    IntTree.preOrderTraversal();
+    std::cout << "\n inOrderTraversal: ";
+    IntTree.inOrderTraversal();
+    std::cout << "\n postOrderTraversal: ";
+    IntTree.postOrderTraversal();
+
     return 0;
 }
