90802d42aa25df2548337f841d7652b2bc4b7f1f
Jasper Blank <j.j.blank@student.utwente.nl>
2024-03-14 17:25:20 +0100

    Solving 5.2

--- Assignment5/assignment5.1/src/DrawContourMarching.cpp
+++ Assignment5/assignment5.1/src/DrawContourMarching.cpp
@@ -8,8 +8,7 @@
 
 #include "DrawContourMarching.h"
 #include <queue>
-#include <vector>
-#include <algorithm>
+#include <set>
 
 // draw contour
 void DrawContourMarching::drawContour(float threshold) {    
@@ -18,7 +17,7 @@ void DrawContourMarching::drawContour(float threshold) {
     Point Firstpoint = findCurve(threshold);
 
     std::queue<Point> worklist;
-    std::vector<Point> visitedlist;
+    std::set<Point> visitedlist;
 
     worklist.push(Firstpoint);
 
@@ -29,32 +28,55 @@ void DrawContourMarching::drawContour(float threshold) {
         //Removing element from queue
         worklist.pop();
 
-    
-   
-    
-    
-        
-        /*
-        //Creating the pixel to the left and adding it to the work list
-        if(p.x > -100 ){
-        Point leftp = Point(p.x-1,p.y);
-        worklist.push(leftp);
+
+        //Check if this pixel has already been visited
+        if (visitedlist.find(p) == visitedlist.end()){
+        visitedlist.insert(p);  
+        } else {
+            continue;
         }
-        */
-
-        //Drawing the current pixel
-        ui->drawPixel(p.x, p.y);
-
-        visitedlist.push_back(p);  
 
         
-    }
-    
-    
+        
+        bool Ptopleft = blob->potential(p.x - 0.5, p.y -0.5) >= threshold;
+        bool Ptopright = blob->potential(p.x + 0.5, p.y -0.5) >= threshold;
+        bool Pbottomright = blob->potential(p.x + 0.5, p.y +0.5) >= threshold;
+        bool Pbottomleft = blob->potential(p.x - 0.5, p.y +0.5) >= threshold;
 
-    
-    
 
+
+        //Check if the pixel is on the curve
+        if (!(Ptopleft == Ptopright && Ptopright == Pbottomright && Pbottomright == Pbottomleft)) {
+
+            //Drawing the current pixel
+            ui->drawPixel(p.x, p.y);
+
+            // Instantiating the points around this point
+            Point pleft = Point(p.x - 1, p.y);
+            Point pright = Point(p.x + 1, p.y);
+            Point pup = Point(p.x, p.y + 1);
+            Point pdown = Point(p.x, p.y - 1);
+
+            // Instantiating diagonal points
+            Point ptopright = Point(p.x + 1, p.y - 1);
+            Point pbottomright = Point(p.x + 1, p.y + 1);
+            Point pbottomleft = Point(p.x - 1, p.y + 1);
+            Point ptopleft = Point(p.x - 1, p.y - 1);
+
+            // Adding them to the worklist
+            worklist.push(pleft);
+            worklist.push(pright);
+            worklist.push(pup);
+            worklist.push(pdown);
+            worklist.push(ptopright);
+            worklist.push(pbottomright);
+            worklist.push(pbottomleft);
+            worklist.push(ptopleft);
+
+            
+
+        }    
+    }    
 }
 
 
