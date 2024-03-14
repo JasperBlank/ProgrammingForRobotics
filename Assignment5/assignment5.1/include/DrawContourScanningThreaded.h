839ff9d178569deaf7bebf1860e4ebda5c60f161
Jasper Blank <j.j.blank@student.utwente.nl>
2024-03-14 10:57:12 +0100

    All of the things, alll of the time

--- Assignment5/assignment5.1/include/DrawContourScanningThreaded.h
+++ Assignment5/assignment5.1/include/DrawContourScanningThreaded.h
@@ -1,9 +1,9 @@
 //==============================================================
-// Filename    : 
-// Authors     : 
-// Group       :
-// License     :  N.A. or opensource license like LGPL
-// Description : 
+// Filename    : DrawContourScanningThreaded.h
+// Authors     : Jasper Blank & Lucas Koreeda
+// Group       : 8
+// License     : N.A. or opensource license like LGPL
+// Description : Header for the Multithreaded Scanning algorithm
 //==============================================================
 
 // DrawContourScanningThreaded class derived from class
@@ -11,17 +11,28 @@
 #ifndef DRAWCONTOURSCANNINGTHREADED_H
 #define DRAWCONTOURSCANNINGTHREADED_H
 
-#include "DrawContour.h" // DrawContour class declaration
+#include "DrawContourScanning.h" // DrawContour class declaration+
+#include <thread>
+#include <vector>
+#include <mutex>
 
 // DrawContourScanningThreaded class definition
-class DrawContourScanningThreaded : public DrawContour
+class DrawContourScanningThreaded : public DrawContourScanning
 {
+
+private:
+    std::mutex myMutex; // used for synchronization
+
 public:
     // constructor
-    DrawContourScanningThreaded(UI &ui, Blob &blob): DrawContour(ui, blob) {};
+    DrawContourScanningThreaded(UI &ui, Blob &blob): DrawContourScanning(ui, blob) {};
 
     // draw contour
     virtual void drawContour(float threshold);
+
+    // draw one row
+    void printRow (const float *y, float threshold);
+
 }; // end class DrawContourScanningThreaded
 
 #endif
--- Assignment5/assignment5.1/src/DrawContourScanningThreaded.cpp
+++ Assignment5/assignment5.1/src/DrawContourScanningThreaded.cpp
@@ -1,17 +1,34 @@
 //==============================================================
-// Filename    : 
-// Authors     : 
-// Group       :
-// License     :  N.A. or opensource license like LGPL
-// Description : 
+// Filename    : DrawContourScanningThreaded.cpp
+// Authors     : Jasper Blank & Lucas Koreeda
+// Group       : 8
+// License     : N.A. or opensource license like LGPL
+// Description : Multithreaded Scanning algorithm
 //==============================================================
 
 #include "DrawContourScanningThreaded.h" 
 
 // draw contour
 void DrawContourScanningThreaded::drawContour(float threshold) {
+    //MENTION EDUCATIVE.IO MULITHREADING STRUCTURE
+    std::vector<std::thread> threads; // create multiple threads
 
-    // YOUR CODE HERE
+    for (float yPoint = yTop+1; yPoint <= yBottom; yPoint++){
+        // MENTION CHAT GPT FOR THIS LINE 
+        threads.push_back(std::thread([this, yPoint, threshold]() { this->printRow(&yPoint, threshold); }));  
+    }
 
-    ui->drawPixel(10, 0); // Dummy code only
+    for (auto &th : threads){
+        th.join();
+    }
+}
+
+void DrawContourScanningThreaded::printRow (const float *y, float threshold){
+    // x is only inside this function
+    for (float x = xLeft+1; x <= xRight; x++){ 
+            if (isDifferent(&x, y, threshold) == true){
+                std::lock_guard<std::mutex> lock(myMutex); // synchronization
+                ui->drawPixel(x, *y); 
+            }
+    }
 }
